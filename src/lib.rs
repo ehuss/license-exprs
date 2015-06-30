bf9f68ca7512a361ed1c90810baa156a8281b947
Without Boats <lee@libertad.ucsd.edu>
2015-06-30 01:46:32 -0700

    Implemented Display and Error for error output types.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,6 @@
+use std::error::Error;
+use std::fmt;
+
 mod spdx;
 
 use self::LicenseExpr::*;
@@ -6,7 +9,18 @@ use self::LicenseExpr::*;
 pub enum LicenseExpr<'a> {
     License(&'a str),
     Exception(&'a str),
-    With, And, Or
+    And, Or, With, 
+}
+
+impl<'a> fmt::Display for LicenseExpr<'a> {
+    fn fmt(&self, format: &mut fmt::Formatter) -> Result<(), fmt::Error> {
+        match *self {
+            With => format.write_str("WITH"),
+            And  => format.write_str("AND"),
+            Or   => format.write_str("OR"),
+            License(info) | Exception(info) => format.write_str(info),
+        }
+    }
 }
 
 #[derive(Debug, Clone, Copy)]
@@ -15,28 +29,47 @@ pub enum ParseError<'a> {
     InvalidStructure(LicenseExpr<'a>)
 }
 
+impl<'a> fmt::Display for ParseError<'a> {
+    fn fmt(&self, format: &mut fmt::Formatter) -> Result<(), fmt::Error> {
+        match *self {
+            ParseError::UnknownLicenseId(info)
+                => format.write_fmt(format_args!("{}: {}", self.description(), info)),
+            ParseError::InvalidStructure(info)
+                => format.write_fmt(format_args!("{}: {}", self.description(), info)),
+        }
+    }
+}
+
+impl<'a> Error for ParseError<'a> {
+    fn description(&self) -> &str {
+        match *self {
+            ParseError::UnknownLicenseId(_) => "unknown license or other term",
+            ParseError::InvalidStructure(_) => "invalid license expression",
+        }
+    }
+}
+
 pub fn validate_license_expr(license_expr: &str) -> Result<&str, ParseError> {
     license_expr.split_whitespace().map(|word| match word {
-        "WITH" => Ok(With),
         "AND"  => Ok(And),
         "OR"   => Ok(Or),
+        "WITH" => Ok(With),
         _ if spdx::LICENSES.contains(&word)   => Ok(License(word)),
         _ if spdx::EXCEPTIONS.contains(&word) => Ok(Exception(word)),
         _ => Err(ParseError::UnknownLicenseId(word))
     }).fold(Ok(Or), |prev, word| match (prev, word) {
-        (_, Err(_)) => word,
-        (Err(_), _) => prev,
+        (err @ Err(_), _) | (_, err @ Err(_)) => err,
         (Ok(License(_)), Ok(With))
             | (Ok(License(_)), Ok(And))
             | (Ok(License(_)), Ok(Or))
             | (Ok(Exception(_)), Ok(And))
             | (Ok(Exception(_)), Ok(Or))
-            | (Ok(With), Ok(Exception(_)))
             | (Ok(And), Ok(License(_)))
             | (Ok(Or), Ok(License(_)))
+            | (Ok(With), Ok(Exception(_)))
             => word,
         _ => Err(ParseError::InvalidStructure(word.unwrap()))
-    }).map(|expr| { println!("{:?}", expr); license_expr })
+    }).map(|_| license_expr)
 }
 
 #[cfg(test)]
